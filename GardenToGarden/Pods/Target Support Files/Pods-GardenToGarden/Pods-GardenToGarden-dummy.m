ac6e61039e071859a8384d1f0d75bbccb5d04bb5
Clinton Buie <clinton@soulswipe.com>
2016-03-05 16:15:17 -0800

    Added the CocoaPods

---
+++ GardenToGarden/Pods/Target Support Files/Pods-GardenToGarden/Pods-GardenToGarden-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GardenToGarden : NSObject
+@end
+@implementation PodsDummy_Pods_GardenToGarden
+@end
---
+++ GardenToGarden/Pods/Target Support Files/Pods-GardenToGardenUITests/Pods-GardenToGardenUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GardenToGardenUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GardenToGardenUITestsVersionString[];
+
